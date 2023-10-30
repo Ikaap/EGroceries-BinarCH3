f0c53e8b4b66ad8f587ad0f950fb935977ca928e
Ikap <ikapurwanti144@gmail.com>
2023-10-30 23:57:56 +0700

    refactor injection to hilt

--- app/src/main/java/com/catnip/egroceries/App.kt
+++ app/src/main/java/com/catnip/egroceries/App.kt
@@ -1,27 +1,16 @@
 package com.catnip.egroceries
 
 import android.app.Application
-import com.catnip.egroceries.di.AppModules
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
 /**
 Written with love by Muhammad Hermas Yuda Pamungkas
 Github : https://github.com/hermasyp
  **/
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        initKoin()
-    }
-
-    private fun initKoin() {
-        startKoin {
-            androidLogger()
-            androidContext(this@App)
-            modules(AppModules.modules)
-        }
     }
 }
---
+++ app/src/main/java/com/catnip/egroceries/di/hilt/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.catnip.egroceries.di.hilt
+
+import android.content.Context
+import com.catnip.egroceries.data.network.api.service.EGroceriesApiService
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideChuckerInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
+        return ChuckerInterceptor(context)
+    }
+
+    @Singleton
+    @Provides
+    fun provideEGroceriesApiService(chuckerInterceptor: ChuckerInterceptor): EGroceriesApiService {
+        return EGroceriesApiService.invoke(chuckerInterceptor)
+    }
+}
